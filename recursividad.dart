7ed47e73b87b4059364b774e0a6aa3476e7a601f
LuissyS77 <luissyhaimar1@gmail.com>
2025-05-13 06:58:29 -0500

    cua commt

--- recursividad.dart
+++ recursividad.dart
@@ -7,12 +7,12 @@ int numero = int.parse(stdin.readLineSync()!);
 int resultado = factorial(numero);
 print("El factorial de $numero es: $resultado");
 
-print("Ingresa un numero con finonacci: ");
+print("Ingresa un numero con finonacci: ");   
 int numero2 = int.parse(stdin.readLineSync()!);
-resultado = fibonacci(numero2);
-print("El fibonacci de $numero2 es: $resultado");
-}
+int resultado2 = fibonacci(numero2);
+print("El factorial con fibonacci de $numero2 es: $resultado2");
 
+}
 int fibonacci(int n) {
   if (n == 0) {
     return 0;
@@ -23,16 +23,29 @@ int fibonacci(int n) {
   }
 }
 
+// int fibonacci(int n) {
+//   if (n == 0) {
+//     return 0;
+//   } else if (n == 1) {
+//     return 1;
+//   } else {
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+// }
+
+// int factorial(int numero) {
+//   if (numero == 0 || numero == 1) {
+//     return 1;
+//   } else {
+//     return numero*factorial(numero - 1);
+//   } 
+// }
 int factorial(int numero) {
-  if (numero == 0 || numero == 1) {
-    return 1;
-  } else {
-    return numero*factorial(numero - 1);
-  } 
+    if (numero == 0 || numero == 1) {
+        return 1;
+    } else {
+        return numero * factorial(numero - 1);
+    }
 }
 
-int factorial2(int n){
-  for (var i = 0; i < n ; i++) {
-    return i * factorial2(i - 1);
-  }
-}
+
